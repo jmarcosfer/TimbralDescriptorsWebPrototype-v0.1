b31724bae81e243e13e33322b7fbb7b3263a51a9
Jorge Marcos <jorge.marcosfer@gmail.com>
2020-05-16 01:07:58 +0200

    separate queries for user and for data analysis

--- app.py
+++ app.py
@@ -14,15 +14,28 @@ metadata_fields = ["id", "name", "tags", "username", "description","duration", "
 timbral_descriptors = ["ac_brightness", "ac_depth", "ac_hardness", "ac_roughness", "ac_boominess", "ac_warmth", "ac_sharpness"]
 
 # Helper functions
-def query_freesound(query):
+def query_freesound(query, size):
+	
 	pager = fs_client.text_search(
 		query=query,
 		fields=",".join(metadata_fields),
 		group_by_pack=1,
-		page_size=20,
+		page_size=size,
 		filter="duration:[* TO 29]"
 	)
-	return [sound for sound in pager if (sound.ac_analysis and (sound.duration <= 30.0))]
+
+	return pager
+
+def scan_pager(pager, max_pages):
+	page_aggregate = [sound for sound in pager if sound.ac_analysis]
+
+	for p in range(max_pages):
+		next_page = pager.next_page()
+		if next_page is not None:
+			page_aggregate += [s for s in next_page if s.ac_analysis]
+		elif next_page is None:
+			break
+	return page_aggregate
 
 def make_pandas_record(fs_object):
 	sound_dict = fs_object.as_dict()
@@ -36,13 +49,8 @@ def make_pandas_record(fs_object):
 			record[descriptor] = "NaN"
 	return record
 
-def get_results(query):
-	sounds = query_freesound(query) 
-	results_df = pd.DataFrame([ make_pandas_record(s) for s in sounds ])
-	return results_df
 
-
-
+# start app:
 app = Flask(__name__, static_url_path="/media", static_folder='media')
 
 @app.route('/')
@@ -54,14 +62,23 @@ def search():
 	query_string = request.args.get('q')
 
 	# make query & results analysis logic:
-	results = get_results(query_string)
+	results_pager = query_freesound(query_string, 20)
 
-	descriptor_stats = results.loc[:, timbral_descriptors].describe([0.25, 0.5, 0.75])
+	aggregate_results = scan_pager(query_freesound(query_string, 150), 4)
 
-	# Now, from <results> dataframe, use:
+	aggregate_results_df = pd.DataFrame([ make_pandas_record(s) for s in aggregate_results ])
+
+	if results_pager.count is not 0:
+
+		descriptor_stats = aggregate_results_df.loc[:, timbral_descriptors].describe([0.25, 0.5, 0.75])
+
+		# Now, from <results> dataframe, use:
+		
+		# - descriptor stats to determine slider ranges, steps, and scale warp
+		
+		embed_links = ["https://freesound.org/embed/sound/iframe/{0}/simple/medium/".format(sound.id) for sound in results_pager]
+
+		return render_template('results.html', query_string=query_string, results=embed_links, descriptor_stats=descriptor_stats.to_dict())
 	
-	# - descriptor stats to determine slider ranges, steps, and scale warp
-	
-	embed_links = ["https://freesound.org/embed/sound/iframe/{0}/simple/medium/".format(sound_id) for sound_id in results.loc[:,"id"].tolist()]
-
-	return render_template('results.html', query_string=query_string, results=embed_links, descriptor_stats=descriptor_stats.to_dict())
+	elif results_pager.count is 0:
+		return render_template('failure.html', query_string=query_string)
