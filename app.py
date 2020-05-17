2a761dd29cb5a6aecf3d5c0c0118109bb12fe04b
Jorge Marcos <jorge.marcosfer@gmail.com>
2020-05-17 18:44:14 +0200

    freesound-API-dependent filtering working (but slow)

--- app.py
+++ app.py
@@ -14,14 +14,19 @@ metadata_fields = ["id", "name", "tags", "username", "description","duration", "
 timbral_descriptors = ["ac_brightness", "ac_depth", "ac_hardness", "ac_roughness", "ac_boominess", "ac_warmth", "ac_sharpness"]
 
 # Helper functions
-def query_freesound(query, size):
-	
+def format_name(descriptor_name):
+	descriptor_name = descriptor_name.replace("ac_", "")
+	return descriptor_name.title()
+
+def query_freesound(query, size, descriptor_filter):
+	filters = "duration:[* TO 29]"
+	if descriptor_filter is not None: filters += descriptor_filter
 	pager = fs_client.text_search(
 		query=query,
 		fields=",".join(metadata_fields),
 		group_by_pack=1,
 		page_size=size,
-		filter="duration:[* TO 29]"
+		filter=filters
 	)
 
 	return pager
@@ -29,13 +34,18 @@ def query_freesound(query, size):
 def scan_pager(pager, max_pages):
 	page_aggregate = [sound for sound in pager if sound.ac_analysis]
 
-	for p in range(max_pages):
-		next_page = pager.next_page()
-		if next_page is not None:
-			page_aggregate += [s for s in next_page if s.ac_analysis]
-		elif next_page is None:
-			break
-	return page_aggregate
+	if max_pages >= 1: return page_aggregate
+	else:
+		for p in range(max_pages):
+			try:
+				next_page = pager.next_page()
+			except ValueError:
+				next_page = None
+			if next_page is not None:
+				page_aggregate += [s for s in next_page if s.ac_analysis]
+			elif next_page is None:
+				break
+		return page_aggregate
 
 def make_pandas_record(fs_object):
 	sound_dict = fs_object.as_dict()
@@ -60,25 +70,28 @@ def index():
 @app.route('/search')
 def search():
 	query_string = request.args.get('q')
+	descriptor_filter = request.args.get('f')
 
+	print("Received query")
 	# make query & results analysis logic:
-	results_pager = query_freesound(query_string, 20)
-
-	aggregate_results = scan_pager(query_freesound(query_string, 150), 4)
-
+	results_pager = query_freesound(query_string, 20, descriptor_filter)
+	print("Received user results")
+	analysis_pager = query_freesound(query_string, 150, descriptor_filter)
+	aggregate_results = scan_pager(analysis_pager, 1)
+	print(f"Received analysis results ({len(aggregate_results)})")
 	aggregate_results_df = pd.DataFrame([ make_pandas_record(s) for s in aggregate_results ])
-
+	print("Put analysis results into dataframe")
 	if results_pager.count is not 0:
 
 		descriptor_stats = aggregate_results_df.loc[:, timbral_descriptors].describe([0.25, 0.5, 0.75])
-
+		print("Calculated stats!")
 		# Now, from <results> dataframe, use:
 		
 		# - descriptor stats to determine slider ranges, steps, and scale warp
 		
 		embed_links = ["https://freesound.org/embed/sound/iframe/{0}/simple/medium/".format(sound.id) for sound in results_pager]
 
-		return render_template('results.html', query_string=query_string, results=embed_links, descriptor_stats=descriptor_stats.to_dict())
+		return render_template('results.html', query_string=query_string, results=embed_links, descriptor_stats=descriptor_stats.to_dict(), format_name=format_name)
 	
 	elif results_pager.count is 0:
 		return render_template('failure.html', query_string=query_string)
