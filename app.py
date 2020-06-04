ad2612512c0de1dc3cd4cbf2a47f2415d8e91676
Jorge Marcos <jorge.marcosfer@gmail.com>
2020-06-05 00:30:03 +0200

    added histograms over sliders

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ fs_client = freesound.FreesoundClient()
 fs_client.set_token(FS_API_KEY)
 
 # Globals:
-metadata_fields = ["id", "name", "tags", "username", "description","duration", "ac_analysis"]
+metadata_fields = ["id", "name", "duration", "ac_analysis"]
 timbral_descriptors = ["ac_brightness", "ac_depth", "ac_hardness", "ac_roughness", "ac_boominess", "ac_warmth", "ac_sharpness"]
 
-# Helper functions
+##################################################################################
+# Helper Functions:
 def format_name(descriptor_name):
 	descriptor_name = descriptor_name.replace("ac_", "")
 	return descriptor_name.title()
@@ -59,9 +60,13 @@ def make_pandas_record(fs_object):
 			record[descriptor] = "NaN"
 	return record
 
-
-# start app:
-app = Flask(__name__, static_url_path="/media", static_folder='media')
+def quantize(x):
+	if (not np.isnan(x)) and (type(x) is not str):
+		return int(round(x))
+		
+##################################################################################
+# START APP:
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -75,7 +80,7 @@ def search():
 	print("Received query")
 	# make query & results analysis logic:
 	results_pager = query_freesound(query_string, 20, descriptor_filter)
-	print("Received user results")
+	print(f"Received user results ({len(results_pager.results)})")
 	analysis_pager = query_freesound(query_string, 150, descriptor_filter)
 	aggregate_results = scan_pager(analysis_pager, 2)
 	print(f"Received analysis results ({len(aggregate_results)})")
@@ -85,13 +90,21 @@ def search():
 
 		descriptor_stats = aggregate_results_df.loc[:, timbral_descriptors].describe([0.25, 0.5, 0.75])
 		print("Calculated stats!")
-		# Now, from <results> dataframe, use:
-		
-		# - descriptor stats to determine slider ranges, steps, and scale warp
+
+		descriptor_dist = {}
+		for desc in timbral_descriptors:
+			quantized_results = aggregate_results_df.loc[:, desc].apply(quantize)
+			dist = quantized_results.value_counts(sort=False)
+			dist_array = np.zeros((101,), dtype=int) # include values 0 and 100 both
+			for i in dist.index:
+				i = int(i)
+				dist_array[i] = dist.loc[i] 
+			descriptor_dist[desc] = dist_array.tolist()
+		print("Calculated full distributions")
 		
 		result_ids = [sound.id for sound in results_pager]
 
-		return render_template('results.html', query_string=query_string, results=result_ids, descriptor_stats=descriptor_stats.to_dict(), format_name=format_name)
+		return render_template('results.html', query_string=query_string, results=result_ids, descriptor_stats=descriptor_stats.to_dict(), format_name=format_name, descriptor_dist=descriptor_dist)
 	
 	elif results_pager.count is 0:
 		return render_template('failure.html', query_string=query_string)
---
+++ static/js/main.js
@@ -0,0 +1,109 @@
+// *************************************************************************** //
+// RESULTS CODE:
+let resultsDiv = document.getElementById("content_full");
+let results = JSON.parse(resultsDiv.dataset.results);
+
+results.forEach((result, index) => {
+	resultsDiv.innerHTML += `<div class="sample_player_small">
+				<iframe frameborder="0" scrolling="no" src="https://freesound.org/embed/sound/iframe/${result}/simple/medium/" width="481" height="86"></iframe>
+			</div>`
+})
+// *************************************************************************** //
+
+
+// *************************************************************************** //
+// SLIDERS AND CHARTS CODE:
+let sideBar = document.getElementById('sidebar');
+let descriptorStats = JSON.parse(sideBar.dataset.stats);
+let descriptorDist = JSON.parse(sideBar.dataset.dist);
+
+let sliderDivs = Array.from(document.getElementsByClassName("slider-block"));
+
+let sliders = {};
+let histLabels = [...Array(101).keys()]; // range for all AC descriptors 0 - 100
+
+// Utility functions:
+function createHistogram(ctx, label, data) {
+	return new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: histLabels,
+			datasets: [{
+				label: label, // from variable
+				data: data, // from variable
+				borderWidth: 1,
+				backgroundColor: 'rgba(0, 0, 0, 0.8)'
+			}]
+		},
+		options: {
+			legend: {
+				display: false
+			},
+			scales: {
+				yAxes: [{
+					display: false
+				}]
+			}
+		}
+	});
+}
+
+function updateColorBars() {
+	// where 'this' refers to the sliderDiv.noUiSlider which fired the event that called this handler
+	let sliderRange = this.get();
+	
+	sliders[this.target.id].chart.data.datasets[0].backgroundColor = function(context) {
+		let index = context.dataIndex;
+		if (index >= sliderRange[0] && index <= sliderRange[1]) {
+			return '#ce1830';
+		} else {
+			return '#ADADAD';
+		}
+	};
+
+	sliders[this.target.id].chart.update();
+}
+
+// Main Loop:
+sliderDivs.forEach((sliderDiv, index) => {
+	let descriptorName = sliderDiv.id;
+	let sliderStats = descriptorStats[descriptorName];
+	let cvsCtx = document.querySelector(`.slider-canvas#${ descriptorName }`).getContext('2d');
+
+	sliders[descriptorName] = {
+		div: sliderDiv,
+		stats: sliderStats,
+		chart: createHistogram(cvsCtx, descriptorName, descriptorDist[descriptorName])
+	};
+
+	noUiSlider.create(sliderDiv, {
+		start: [sliderStats.min, sliderStats.max],
+		connect: true,
+		range: {
+			'min': 0,
+			'max': 100
+		}
+	});
+
+	// add event handler function:
+	
+	sliderDiv.noUiSlider.on("update", updateColorBars);
+});
+
+
+// *************************************************************************** //
+/*	function() {
+	sliderValues = sliderDiv.noUiSlider.get();
+	console.log(`${descriptorName}: ${sliderValues}`);
+	let searchURL = `${window.location.origin}/search?q=${queryString}&f=${descriptorName}%3A%5B${sliderValues[0]} TO ${sliderValues[1]}%5D`
+
+	window.location = searchURL;
+	// let xhr = new XMLHttpRequest();
+	// xhr.addEventListener('load', function() {
+	// 	if (this.status == 200 && this.readyState == 4) {
+	// 		window.location = this.responseURL;
+	// 	}
+	// });
+	// xhr.open("GET", searchURL, true);
+	// xhr.send(null);
+} */
