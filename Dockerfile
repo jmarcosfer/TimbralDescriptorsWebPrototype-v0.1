6e5e0cda9fa25f971ba7e36d1abc22706549c0a0
Jorge Marcos <jorge.marcosfer@gmail.com>
2020-08-04 21:24:43 +0200

    changed flask devserver to waitress WSGI

--- Dockerfile
+++ Dockerfile
@@ -21,4 +21,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD [ "flask", "run", "--host", "0.0.0.0", "--port", "8080"]
+CMD [ "python", "app.py"]
--- app.py
+++ app.py
@@ -184,3 +184,7 @@ def collect_feedback():
 @app.route('/form_success', methods=['GET'])
 def form_success():
 	return render_template('form_success.html')
+
+if __name__ == '__main__':
+	from waitress import serve
+	serve(app, host='0.0.0.0', port='8080')
